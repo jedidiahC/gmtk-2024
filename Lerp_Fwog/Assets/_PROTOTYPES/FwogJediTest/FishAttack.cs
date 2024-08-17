ef019bfe6a17a40e5f4e274bd4bc59561866e425
KlearVacuum <73681455+KlearVacuum@users.noreply.github.com>
2024-08-17 22:37:24 +0800

    Fish and tadpole chase target logic

--- Lerp_Fwog/Assets/_PROTOTYPES/FwogJediTest/FishAttack.cs
+++ Lerp_Fwog/Assets/_PROTOTYPES/FwogJediTest/FishAttack.cs
@@ -5,50 +5,50 @@ using UnityEngine;
 
 public class FishAttack : MonoBehaviour
 {
-    [SerializeField] private TadpoleLife target = null;
+    // [SerializeField] private TadpoleLife target = null;
     [SerializeField] private float speed = 5.0f;
 
-    private void Update()
-    {
-        ChooseClosestTarget();
-        Chase();
-    }
+    //private void Update()
+    //{
+        // ChooseClosestTarget();
+        // Chase();
+    //}
 
-    private void ChooseClosestTarget()
-    {
-        List<TadpoleLife> prey = GameTracker.GetInstance().GetTadpoles();
+    //private void ChooseClosestTarget()
+    //{
+    //    List<TadpoleLife> prey = GameTracker.GetInstance().GetTadpoles();
 
-        if (prey != null)
-        {
-            foreach (var candidateTarget in prey)
-            {
-                if (candidateTarget == null || candidateTarget.IsDead)
-                {
-                    continue;
-                }
+    //    if (prey != null)
+    //    {
+    //        foreach (var candidateTarget in prey)
+    //        {
+    //            if (candidateTarget == null || candidateTarget.IsDead)
+    //            {
+    //                continue;
+    //            }
 
-                if (target == null || target.IsDead || GetDist(candidateTarget) < GetDist(target))
-                {
-                    target = candidateTarget;
-                }
-            }
-        }
-    }
+    //            if (target == null || target.IsDead || GetDist(candidateTarget) < GetDist(target))
+    //            {
+    //                target = candidateTarget;
+    //            }
+    //        }
+    //    }
+    //}
 
-    private void Chase()
-    {
-        if (target == null)
-        {
-            return;
-        }
+    //private void Chase()
+    //{
+    //    if (target == null)
+    //    {
+    //        return;
+    //    }
 
-        transform.position += (target.transform.position - this.transform.position).normalized * speed * Time.deltaTime;
-    }
+    //    transform.position += (target.transform.position - this.transform.position).normalized * speed * Time.deltaTime;
+    //}
 
-    private float GetDist(TadpoleLife life)
-    {
-        return Vector3.Distance(this.transform.position, life.transform.position);
-    }
+    //private float GetDist(TadpoleLife life)
+    //{
+    //    return Vector3.Distance(this.transform.position, life.transform.position);
+    //}
 
 
     void OnCollisionEnter2D(Collision2D col)
@@ -58,7 +58,7 @@ public class FishAttack : MonoBehaviour
         if (victim != null)
         {
             victim.Kill();
-            this.target = null;
+            // this.target = null;
         }
     }
 }
--- Lerp_Fwog/Assets/_PROTOTYPES/Keff/TadpoleMovement.cs
+++ Lerp_Fwog/Assets/_PROTOTYPES/Keff/TadpoleMovement.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum eCreatureType
+{
+    NONE,
+    TADPOLE,
+    FISH
+}
+
 public class TadpoleMovement : MonoBehaviour
 {
+    [SerializeField] private eCreatureType _creatureType;
     private Vector3 _dir;
     [SerializeField] private float _moveForce;
     [SerializeField] private float _maxSpeed;
     [SerializeField] private float _rotateSpeed;
     [Range(0,1)] [SerializeField] private float _rotateDampening;
+    [SerializeField] private float _viewTargetRange;
+    [Range(0,180)] [SerializeField] private float _viewTargetAngle;
 
+    [SerializeField] private Transform _targetTransform;
     private Rigidbody2D _rb;
 
     // steering vectors
@@ -17,11 +28,12 @@ public class TadpoleMovement : MonoBehaviour
     [SerializeField] private float _steeringUpdateInterval;
     [Range(1,36)] [SerializeField] private int _steeringVectorsQty;
     [Range(0,360)] [SerializeField] private int _steeringVectorsMaxAngle;
-    [SerializeField] private float _steeringCircleCastRadius;
+    [Range(0,1)] [SerializeField] private float _steeringCircleCastRadiusRatio;
+    private float _steeringCircleCastRadius;
     [SerializeField] private float _steeringCirclecastDistance;
     // front vector checks the furthest
     [SerializeField] private float _circlecastExtendedDist;
-
+    
     private Vector3 _desiredDir;
     private Vector3[] _steeringVectorsArr;
     private float _currentSteeringUpdateDuration;
@@ -39,8 +51,39 @@ public class TadpoleMovement : MonoBehaviour
         _steeringVectorsArr = new Vector3[_steeringVectorsQty];
         SetSteeringVectors();
         _currentSteeringUpdateDuration = 0;
+        _steeringCircleCastRadius = transform.localScale.x * _steeringCircleCastRadiusRatio;
+
+        if (_creatureType == eCreatureType.TADPOLE) _targetTransform = GameObject.FindGameObjectWithTag("Finish").transform;
+        else if (_creatureType == eCreatureType.FISH) FindTadpoleTarget();
     }
 
+    void Update()
+    {
+        if (!MoveToTargetInSight())
+            Steering();
+    }
+
+    void FixedUpdate()
+    {
+        _desiredDir.Normalize();
+        if (Vector3.Angle(_dir, _desiredDir) > 1)
+        {
+            _dir = Vector3.Lerp(_dir, _desiredDir, _rotateSpeed * Time.fixedDeltaTime);
+            _dir.Normalize();
+            _rb.velocity *= _rotateDampening;
+        }
+        else
+        {
+            _dir = _desiredDir;
+        }
+        float angle = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
+        _rb.rotation = angle;
+
+        if (_rb.velocity.magnitude < _maxSpeed)
+        {
+            _rb.AddForce(_dir * _moveForce * Time.fixedDeltaTime);
+        }
+    }
     void SetSteeringVectors(float inFacingRelativeAngleRad = 0)
     {
         float steeringAngleStep = (float)_steeringVectorsMaxAngle * Mathf.Deg2Rad / (float)(_steeringVectorsQty - 1);
@@ -65,33 +108,6 @@ public class TadpoleMovement : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        Steering();
-    }
-
-    void FixedUpdate()
-    {
-        _desiredDir.Normalize();
-        if (Vector3.Angle(_dir, _desiredDir) > 1)
-        {
-            _dir = Vector3.Lerp(_dir, _desiredDir, _rotateSpeed * Time.fixedDeltaTime);
-            _dir.Normalize();
-            _rb.velocity *= _rotateDampening;
-        }
-        else
-        {
-            _dir = _desiredDir;
-        }
-        float angle = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
-        _rb.rotation = angle;
-
-        if (_rb.velocity.magnitude < _maxSpeed)
-        {
-            _rb.AddForce(_dir * _moveForce * Time.fixedDeltaTime);
-        }
-    }
-
     void Steering()
     {
         _currentSteeringUpdateDuration += Time.deltaTime;
@@ -105,17 +121,54 @@ public class TadpoleMovement : MonoBehaviour
             for (int i = 0; i < _steeringVectorsQty; i++)
             {
                 float raycastDist = _steeringCirclecastDistance + (1 - i / _steeringVectorsQty) * _circlecastExtendedDist;
-                RaycastHit2D hit = Physics2D.CircleCast(transform.position, _steeringCircleCastRadius, _steeringVectorsArr[i], raycastDist);
+                RaycastHit2D hit = Physics2D.CircleCast(transform.position + (_steeringVectorsArr[i] * (_steeringCircleCastRadius * 2 + 0.05f)), _steeringCircleCastRadius, _steeringVectorsArr[i], raycastDist);
                 if (hit.collider == null)
                 {
                     _desiredDir = _steeringVectorsArr[i];
-                    Debug.DrawLine(transform.position, transform.position + _steeringVectorsArr[i], Color.green);
+                    Debug.DrawLine(transform.position, transform.position + _steeringVectorsArr[i], Color.green, 5f);
                     break;
                 }
-                Debug.DrawLine(transform.position, transform.position + _steeringVectorsArr[i], Color.red);
+                Debug.DrawLine(transform.position, transform.position + _steeringVectorsArr[i], Color.red, 5f);
                 if (i == _steeringVectorsQty - 1) _desiredDir = _steeringVectorsArr[i];
             }
             _currentSteeringUpdateDuration = 0;
+        }
+    }
+
+    bool MoveToTargetInSight()
+    {
+        if (_targetTransform == null) return false;
+
+        Vector3 targetDir = (_targetTransform.position - transform.position).normalized;
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position + (targetDir * (_steeringCircleCastRadius * 2 + 0.05f)), _steeringCircleCastRadius, targetDir, _viewTargetRange);
+
+        // I can see my target
+        if (hit.collider != null && hit.collider.transform == _targetTransform)
+        {
+            if (Vector3.Angle(_dir, targetDir) <= _viewTargetAngle)
+            {
+                _desiredDir = targetDir;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void FindTadpoleTarget()
+    {
+        Collider2D[] foundColliders = Physics2D.OverlapCircleAll(transform.position, _viewTargetRange);
+        if (foundColliders == null || foundColliders.Length == 0) return;
+
+        foreach(Collider2D col in foundColliders)
+        {
+            if (col.gameObject.CompareTag("Tadpole"))
+            {
+                if (Vector3.Angle(_dir, (col.transform.position - transform.position).normalized) <= _viewTargetAngle)
+                {
+                    _targetTransform = col.transform;
+                    return;
+                }
+            }
         }
     }
 }
