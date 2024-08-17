5b88f95e7befe341ea30519b25549e9b5e1bf12b
Rayner Tan <raynertanxw@me.com>
2024-08-17 17:16:43 +0800

    Target Area now displays number of hits left.
    - Made use of new target area to construct a new level with scattershot blue blocks.

--- Lerp_Fwog/Assets/_PROTOTYPES/WeighingScales/TargetArea.cs
+++ Lerp_Fwog/Assets/_PROTOTYPES/WeighingScales/TargetArea.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TargetArea : MonoBehaviour {
     [SerializeField] private int _targetGoalNum = 0;
+    [SerializeField] private RectTransform _canvasRectTransform = null;
+    [SerializeField] private TextMeshProUGUI _countLeftText = null;
     private SpriteRenderer _spriteRen = null;
     private int _targetCount = 0;
     private bool _reachedTarget = false;
@@ -14,15 +17,22 @@ public class TargetArea : MonoBehaviour {
 
     void Awake() {
         Debug.Assert(_targetGoalNum > 0, "_targetGoalNum must be > 0");
+        Debug.Assert(_canvasRectTransform != null, "_canvasRectTransform is not assigned");
+        Debug.Assert(_countLeftText != null, "_countLeftText is not assigned");
         _spriteRen = GetComponent<SpriteRenderer>();
         Debug.Assert(_spriteRen != null, "_spriteRen is not found!");
 
         ResetTarget();
+
+        // Force it to be same size so text not skewed.
+        Utils.SetGlobalScale(_canvasRectTransform, new Vector3(0.01f, 0.01f, 0.01f));
     }
+
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Finish") {
             _targetCount++;
+            _countLeftText.text = Mathf.Max(_targetGoalNum - _targetCount, 0).ToString();
             if (_targetCount >= _targetGoalNum) {
                 _reachedTarget = true;
                 _spriteRen.color = _reachedColor;
@@ -35,5 +45,6 @@ public class TargetArea : MonoBehaviour {
         _targetCount = 0;
         _reachedTarget = false;
         _spriteRen.color = _unreachedColor;
+        _countLeftText.text = _targetGoalNum.ToString();
     }
 }
