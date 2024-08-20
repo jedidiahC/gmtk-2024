0d5985ec71463cfade8863bb9669dfa28a0dceac
Rayner Tan <raynertanxw@me.com>
2024-08-21 00:15:23 +0800

    Added fire particles to finale level.

--- Lerp_Fwog/Assets/_Game/Scripts/AstroFrog.cs
+++ Lerp_Fwog/Assets/_Game/Scripts/AstroFrog.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class AstroFrog : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody = null;
+    [SerializeField] private ParticleSystem _fireParticles = null;
+
+    private Vector3 _particleOffset;
 
     void Awake() {
         Debug.Assert(_rigidbody != null, "_rigidbody is not assigned!");
+        Debug.Assert(_fireParticles != null, "_fireParticles is not assigned!");
+    
+        StopFireParticles();
+        _particleOffset = _fireParticles.transform.position - transform.position;
     }
 
-    void FixedUpdate() {
+    void Update() {
+        _fireParticles.transform.position = transform.position + _particleOffset;
+        _fireParticles.transform.rotation = Quaternion.identity;
     }
+
+    public void StartFireParticles() {
+        _fireParticles.Play();
+    }
+
+    public void StopFireParticles() {
+        _fireParticles.Stop();
+        _fireParticles.Clear();
+    } 
 }
